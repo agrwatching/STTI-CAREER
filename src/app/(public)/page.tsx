c24f88b22564d0079698ede74f714af3d220d483
Abdul Japar <Abduljapar84@gmail.com>
2025-09-11 16:41:11 +0700

    menambahkan review

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,12 +1,14 @@
 // src/app/(public)/page.tsx
 import Hero from "@/components/beranda/Hero";
 import Job from "@/components/beranda/Job";
+import Review from "@/components/beranda/Review";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-blue-50">
       <Hero />
       <Job />
+      <Review />
     </main>
   );
 }
