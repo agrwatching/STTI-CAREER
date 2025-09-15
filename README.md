850629073340a5defde123c2801e3e48bf2f07e9
Abdul Japar <Abduljapar84@gmail.com>
2025-09-15 17:24:50 +0700

    update

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ npx create-next-app@latest .
 langkah pertama menggunakan apk ini di lokal
 
 1. npm install
-2. npm run dev (running tanpa api atau local api) atau npm run build (running production api) kalau ingin running dengan api local nyalakan server backendnya.
-   happy coding.
+2. npm run dev (running tanpa api atau local api) atau npm run build (running production api)
+
+kalau ingin running dengan api local nyalakan server backendnya, jika perlu nyalakan server databasenya.
+
+happy coding.
--- src/app/(dashboard)/pelamar/profile/[id]/page.tsx
+++ src/app/(dashboard)/pelamar/profile/[id]/page.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/pelamar/profile/Header";
 import ProfileHeader from "@/components/pelamar/profile/ProfileHeader";
 import Tabs from "@/components/pelamar/profile/Tabs";
 import Biodata from "@/components/pelamar/profile/biodata/Biodata";
-import Pendidikan from "@/components/pelamar/profile/Pendidikan";
+import Pendidikan from "@/components/pelamar/profile/pendidikan/Pendidikan";
 import PengalamanSection from "@/components/pelamar/profile/pengalaman/PengalamanSection";
 import SertifikatSection from "@/components/pelamar/profile/sertifikat/SertifikatSection";
 import KeterampilanSection from "@/components/pelamar/profile/keterampilan/KeterampilanSection";
