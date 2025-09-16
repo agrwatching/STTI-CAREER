04ca2608bc37c3fc50f8c7366634a53c52d1a342
watching <agramaesa123@gmail.com>
2025-09-17 00:34:14 +0700

    bismillah

---
+++ src/app/(dashboard)/pelamar/pengaturan/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-gray-600">
+      <p>Halaman ini masih dalam pengembangan.</p>
+    </div>
+  );
+}
