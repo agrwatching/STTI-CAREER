72f0ca9f6b87dfe09d3f2b654fbc4d1a0cbab80d
watching <agramaesa123@gmail.com>
2025-09-17 09:00:09 +0700

    run

--- src/app/(dashboard)/hr/pelamar/page.tsx
+++ src/app/(dashboard)/hr/pelamar/page.tsx
@@ -3,48 +3,56 @@ import PelamarTable from "@/components/hr/pelamar/PelamarTable";
 export default function PelamarPage() {
   const pelamars = [
     {
+      id: 1,
       nama: "Moh Rizal",
       tanggal: "23-08-2023",
       cv: "/cv/moh-rizal.pdf",
       posisi: "Webdev",
     },
     {
+      id: 2,
       nama: "Sutoto",
       tanggal: "23-08-2023",
       cv: "/cv/sutoto.pdf",
       posisi: "Marketing",
     },
     {
+      id: 3,
       nama: "Sididid",
       tanggal: "23-08-2023",
       cv: "/cv/sididid.pdf",
       posisi: "Frontend",
     },
     {
+      id: 4,
       nama: "Moh Rizal",
       tanggal: "23-08-2023",
       cv: "/cv/moh-rizal.pdf",
       posisi: "Webdev",
     },
     {
+      id: 5,
       nama: "Sutoto",
       tanggal: "23-08-2023",
       cv: "/cv/sutoto.pdf",
       posisi: "Marketing",
     },
     {
+      id: 6,
       nama: "Sididid",
       tanggal: "23-08-2023",
       cv: "/cv/sididid.pdf",
       posisi: "Frontend",
     },
     {
+      id: 7,
       nama: "Sididid",
       tanggal: "23-08-2023",
       cv: "/cv/sididid.pdf",
       posisi: "Frontend",
     },
     {
+      id: 8,
       nama: "Sididid",
       tanggal: "23-08-2023",
       cv: "/cv/sididid.pdf",
