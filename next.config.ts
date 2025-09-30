34e4ff2960ef0761bb96d806d48e9fa0a748d7d5
watchingx <agragemers77@gmail.com>
2025-09-30 16:21:43 +0700

    oke gas"

--- next.config.ts
+++ next.config.ts
@@ -19,17 +19,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "dummyimage.com",
       },
-<<<<<<< HEAD
-      {
-        protocol: "https",
-        hostname: "backendstticareer-123965511401.asia-southeast2.run.app",
-=======
-      // ✅ pakai hostname dari ENV
       {
         protocol: "https",
         hostname: apiUrl,
         pathname: "/uploads/images/**",
->>>>>>> d871fa595a65df6331ad9e44cf6ffc3c6e49ef3e
       },
     ],
   },
