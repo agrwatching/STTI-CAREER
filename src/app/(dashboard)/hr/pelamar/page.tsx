8668e4c018177df11a27e7b730ebf5fdf4f1cb2a
Agra <agramaesakusumah@gmail.com>
2025-10-03 20:15:33 +0700

    STTI-CAREER\src\app\(dashboard)\hr\pelamar

--- src/app/(dashboard)/hr/pelamar/page.tsx
+++ src/app/(dashboard)/hr/pelamar/page.tsx
@@ -1,64 +1,60 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import PelamarTable from "@/components/hr/pelamar/PelamarTable";
 
+type JobApplicant = {
+  id: number;
+  nama: string;
+  tanggal: string;
+  cv: string;
+  posisi: string;
+};
+
 export default function PelamarPage() {
-  const pelamars = [
-    {
-      id: 1,
-      nama: "Moh Rizal",
-      tanggal: "23-08-2023",
-      cv: "/cv/moh-rizal.pdf",
-      posisi: "Webdev",
-    },
-    {
-      id: 2,
-      nama: "Sutoto",
-      tanggal: "23-08-2023",
-      cv: "/cv/sutoto.pdf",
-      posisi: "Marketing",
-    },
-    {
-      id: 3,
-      nama: "Sididid",
-      tanggal: "23-08-2023",
-      cv: "/cv/sididid.pdf",
-      posisi: "Frontend",
-    },
-    {
-      id: 4,
-      nama: "Moh Rizal",
-      tanggal: "23-08-2023",
-      cv: "/cv/moh-rizal.pdf",
-      posisi: "Webdev",
-    },
-    {
-      id: 5,
-      nama: "Sutoto",
-      tanggal: "23-08-2023",
-      cv: "/cv/sutoto.pdf",
-      posisi: "Marketing",
-    },
-    {
-      id: 6,
-      nama: "Sididid",
-      tanggal: "23-08-2023",
-      cv: "/cv/sididid.pdf",
-      posisi: "Frontend",
-    },
-    {
-      id: 7,
-      nama: "Sididid",
-      tanggal: "23-08-2023",
-      cv: "/cv/sididid.pdf",
-      posisi: "Frontend",
-    },
-    {
-      id: 8,
-      nama: "Sididid",
-      tanggal: "23-08-2023",
-      cv: "/cv/sididid.pdf",
-      posisi: "Frontend",
-    },
-  ];
+  const [pelamars, setPelamars] = useState<JobApplicant[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchPelamars = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/applicant`, {
+        headers: {
+        Authorization: `Bearer ${token}`,
+        },
+        cache: "no-store",
+        });
+
+        if (!res.ok) {
+          throw new Error("Terjadi kesalahan saat memuat data");
+        }
+
+        const rawData = await res.json();
+        
+        const transformedData = rawData.map((item: any) => ({
+          id: item.id,
+          nama: item.nama,       
+          tanggal: new Date(item.tanggal).toLocaleDateString("id-ID"),
+          cv: `${process.env.NEXT_PUBLIC_API_URL}/uploads/files/${item.cv}`,
+          posisi: item.posisi,     
+        }));
+
+        setPelamars(transformedData);   
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPelamars();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
